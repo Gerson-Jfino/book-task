68983ab18fec63b2b082fdab1621af95ac79e8b8
Gerson Maoze <gerson.maoze@marrar.co.mz>
2023-08-14 10:28:36 +0200

    fix: change api endpoint

--- resources/js/config/url.js
+++ resources/js/config/url.js
@@ -1,2 +1,2 @@
-// export const URL_BASE = 'http://localhost:8000/api'
-export const URL_BASE = 'http://book-task/api'
+export const URL_BASE = 'http://localhost:8000/api'
+// export const URL_BASE = 'http://book-task/api'
